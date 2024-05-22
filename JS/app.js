1e10653c140d66dc20cbf2217ddad5849a13cb8e
Mario Mata <mmatas1300@gmail.com>
2024-05-22 15:56:23 -0600

    Update PS

--- JS/app.js
+++
@@ -1,9 +0,0 @@
-/*
-    Este es nuestro archivo de JavaScript
-*/
-var myCarousel = document.querySelector('#carouselExampleCaptions')
-var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 6000,  //==============reduci tiempo de velocidad -cynthia
-  wrap: true
-});
-
